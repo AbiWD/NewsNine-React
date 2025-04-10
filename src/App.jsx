6ae40410d2cada722da3d6b9eac6c04fdc494c09
Abhilash <abhilashbangera97@gmail.com>
2025-04-11 02:36:52 +0530

    Change to component based stuct, refractoring, completed NewsNine: )

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,126 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import News from "./components/News";
 import { Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
-export default class App extends Component {
-  pageSize = 5;
+const App = () => {
+  const pageSize = 5;
+  const apiKey = import.meta.env.VITE_NEWS_API;
+  const [progress, setProgress] = useState(0);
 
-  render() {
-    return (
-      <>
-        <NavBar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <News
-                key="general"
-                pageSize={this.pageSize}
-                country="in"
-                category="general"
-              />
-            }
-          />
-          <Route
-            path="/business"
-            element={
-              <News
-                key="business"
-                pageSize={this.pageSize}
-                country="in"
-                category="business"
-              />
-            }
-          />
-          <Route
-            path="/entertainment"
-            element={
-              <News
-                key="entertainment"
-                pageSize={this.pageSize}
-                country="in"
-                category="entertainment"
-              />
-            }
-          />
-          <Route
-            path="/general"
-            element={
-              <News
-                key="general"
-                pageSize={this.pageSize}
-                country="in"
-                category="general"
-              />
-            }
-          />
-          <Route
-            path="/health"
-            element={
-              <News
-                key="health"
-                pageSize={this.pageSize}
-                country="in"
-                category="health"
-              />
-            }
-          />
-          <Route
-            path="/science"
-            element={
-              <News
-                key="science"
-                pageSize={this.pageSize}
-                country="in"
-                category="science"
-              />
-            }
-          />
-          <Route
-            path="/sports"
-            element={
-              <News
-                key="sports"
-                pageSize={this.pageSize}
-                country="in"
-                category="sports"
-              />
-            }
-          />
-          <Route
-            path="/technology"
-            element={
-              <News
-                key="technology"
-                pageSize={this.pageSize}
-                country="in"
-                category="technology"
-              />
-            }
-          />
-        </Routes>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <NavBar />
+      <LoadingBar height={3} color="#f11946" progress={progress} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="general"
+              pageSize={pageSize}
+              country="us"
+              category="general"
+            />
+          }
+        />
+        <Route
+          path="/business"
+          element={
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="business"
+              pageSize={pageSize}
+              country="us"
+              category="business"
+            />
+          }
+        />
+        <Route
+          path="/entertainment"
+          element={
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="entertainment"
+              pageSize={pageSize}
+              country="us"
+              category="entertainment"
+            />
+          }
+        />
+        <Route
+          path="/general"
+          element={
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="general2"
+              pageSize={pageSize}
+              country="us"
+              category="general"
+            />
+          }
+        />
+        <Route
+          path="/health"
+          element={
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="health"
+              pageSize={pageSize}
+              country="us"
+              category="health"
+            />
+          }
+        />
+        <Route
+          path="/science"
+          element={
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="science"
+              pageSize={pageSize}
+              country="us"
+              category="science"
+            />
+          }
+        />
+        <Route
+          path="/sports"
+          element={
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="sports"
+              pageSize={pageSize}
+              country="us"
+              category="sports"
+            />
+          }
+        />
+        <Route
+          path="/technology"
+          element={
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="technology"
+              pageSize={pageSize}
+              country="us"
+              category="technology"
+            />
+          }
+        />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
